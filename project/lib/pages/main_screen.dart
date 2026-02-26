a5295eaa28f260562dfd3fa28b643a4ed0684aeb
Adrian Kramarz <adkra000@gmail.com>
2026-02-26 10:23:50 +0100

    settings screen, nav bar

---
+++ project/lib/pages/main_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'moviebrowser_screen.dart';
+import 'profile_screen.dart';
+import 'settings_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    MoviebrowserScreen(),
+    ProfileScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+
+        onDestinationSelected: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined),
+            selectedIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.movie_outlined),
+            selectedIcon: Icon(Icons.movie),
+            label: 'Browse',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.settings_outlined),
+            selectedIcon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ project/lib/pages/settings_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: const Scaffold(
+        body: Column(
+          children: [
+            Text('Settings'),
+          ],
+        ),
+      ),
+    );
+  }
+}
